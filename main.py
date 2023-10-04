8580c5248d426cd6efbffd8db1c9870b4bbd23fb
Magno Martinez <66977118+Magno-12@users.noreply.github.com>
2023-10-04 12:28:17 -0500

    refactoring and implementation of a database

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import JSONResponse
+
 from pydantic import BaseModel, EmailStr
 from typing import List, Optional
 
-app = FastAPI()
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 class UserModel(BaseModel):
     first_name: str
     last_name: str
     email: EmailStr
-    password: str
+    password: Optional[str]
 
 class AddressModel(BaseModel):
     street: str
@@ -20,39 +24,88 @@ class UserWithAddressesModel(BaseModel):
     user: UserModel
     addresses: List[AddressModel]
 
-db = []
+DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class UserModelDB(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+
+class AddressModelDB(Base):
+    __tablename__ = "addresses"
+    id = Column(Integer, primary_key=True, index=True)
+    street = Column(String)
+    city = Column(String)
+    country = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
 
 @app.post("/users/", response_model=UserModel)
 async def create_user(user_with_addresses: UserWithAddressesModel):
-    email_exists = any(user['email'] == user_with_addresses.user.email for user in db)
+    db = SessionLocal()
+    email_exists = db.query(UserModelDB).\
+        filter(UserModelDB.email == user_with_addresses.user.email).\
+                first()
+    db.close()
     if email_exists:
-        raise HTTPException(status_code=400, detail="Email is already registered")
+        raise HTTPException(
+            status_code=400, detail="Email is already registered"
+        )
 
     user_data = user_with_addresses.user.model_dump()
-    user_data["addresses"] = [addr.model_dump() for addr in user_with_addresses.addresses]
-    db.append(user_data)
-    user_dict = user_with_addresses.user.model_dump()
+    addresses_data = [
+        addr.model_dump() for addr in user_with_addresses.addresses
+        ]
+
+    db = SessionLocal()
+    db_user = UserModelDB(**user_data)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    for address_data in addresses_data:
+        address_data["user_id"] = db_user.id
+        db_address = AddressModelDB(**address_data)
+        db.add(db_address)
+
+    db.commit()
+    db.close()
+
     response_data = {
         "message": "User created successfully",
-        "user": user_dict
+        "user": user_with_addresses.user.\
+                model_dump(exclude={"password"}
+                )
     }
     return JSONResponse(content=response_data)
 
 @app.get("/users/", response_model=List[UserModel])
-async def get_users_by_country(country: str = Query(..., title="Country", min_length=3)):
-    users_in_country = []
-    for user in db:
-        if any(addr['country'] == country for addr in user['addresses']):
-            user_copy = user.copy()
-            user_copy.pop("password")
-            users_in_country.append(UserModel(**user))
+async def get_users_by_country(
+    country: str = Query(..., title="Country", min_length=3)):
+    db = SessionLocal()
+    addresses = db.query(AddressModelDB).\
+        filter(AddressModelDB.country == country).all()
+    user_ids = [address.user_id for address in addresses]
+    users_in_country = db.query(UserModelDB).\
+        filter(UserModelDB.id.in_(user_ids)).all()
+    db.close()
     if not users_in_country:
-        raise HTTPException(status_code=404, detail="No users found in the specified country")
-    return users_in_country
-
-@app.exception_handler(HTTPException)
-async def http_exception_handler(request, exc):
-    return JSONResponse(
-        status_code=exc.status_code,
-        content={"message": exc.detail}
-    )
+        raise HTTPException(
+            status_code=404, detail="No users found in the specified country")
+    users_data = [{"first_name": user.first_name,
+                   "last_name": user.last_name,
+                   "email": user.email, "password": None}
+                   for user in users_in_country
+                ]
+    return users_data
